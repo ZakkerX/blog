30b14f6b1bb5bc58c1a0d59dd1a1b0929d7568b1
Ilia Sorokin <zakkerx@yandex.ru>
2019-03-18 15:00:06 +0700

    add model post

--- database.js
+++ database.js
@@ -2,17 +2,17 @@ const config = require('./config');
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  return new Promise((resolve, reject) => {
-    mongoose.Promise = global.Promise;
-    mongoose.set('debug', true);
+	return new Promise((resolve, reject) => {
+		mongoose.Promise = global.Promise;
+		mongoose.set('debug', true);
 
-    mongoose.connection
-      .on('error', error => reject(error))
-      .on('close', () => console.log('Database connection closed.'))
-      .once('open', () => resolve(mongoose.connections[0]));
+		mongoose.connection
+			.on('error', error => reject(error))
+			.on('close', () => console.log('Database connection closed.'))
+			.once('open', () => resolve(mongoose.connections[0]));
 
-    mongoose.connect(config.MONGO_URL, {
-      useMongoClient: true
-    });
-  });
+		mongoose.connect(config.MONGO_URL, {
+			useNewUrlParser: true
+		});
+	});
 };
