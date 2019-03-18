38021f06ebcee95389e8a8f17b67f5feb102e213
Ilia Sorokin <zakkerx@yandex.ru>
2019-03-18 21:39:21 +0700

    Migrate the template to the main project, add static hash styles and scripts

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path')
 const app = express();
+const staticAsset = require('static-asset');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+app.use(staticAsset(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(
 	'/javascripts',
 	express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
